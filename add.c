1027fc38acc0ed69ce16f34020398d466c73ea9a
juin piyada <juinpiyada1@gmail.com>
2024-11-12 17:01:37 +0530

    juin

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>//header file for standerd input output
+
+void main()//a non return main
+{
+  int a,b,c;//variable declaed
+  printf("Enter the First Number :");//print message
+scanf("%d",&a);//scannig
+printf("Enter the Secound Number :");
+scanf("%d",&b);
+c=a+b;//addition logic
+printf("The Addition of Two Numbers Is %d",c);//output
+}
